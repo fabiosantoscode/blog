4d181a4e4c543fa62d122a8de87de59d6d123d85
Fábio Santos <fabiosantosart@gmail.com>
2020-02-27 16:07:00 +0000

    TAP reporter for tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,19 @@
+const tapReporter = process.env.CI
+  ? {
+      reporters: [
+        [
+          'jest-tap-reporter',
+          {
+            logLevel: 'ERROR',
+            showInternalStackTraces: true
+            //filePath: "filename.tap"
+          }
+        ]
+      ]
+    }
+  : {};
+
 module.exports = {
-  testPathIgnorePatterns: ['.cache']
+  testPathIgnorePatterns: ['.cache'],
+  ...tapReporter
 };
