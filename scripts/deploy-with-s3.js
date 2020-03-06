cddc1591e1120c3d9aa635c8345dd4c9b43a8f12
Fábio Santos <fabiosantosart@gmail.com>
2020-03-06 14:33:59 +0000

    update base code

--- scripts/deploy-with-s3.js
+++ scripts/deploy-with-s3.js
@@ -30,14 +30,15 @@ const cacheDir = path.join(rootDir, '.cache');
 const publicDir = path.join(rootDir, 'public');
 const s3Prefix = HEROKU_PR_NUMBER ? `pulls/${HEROKU_PR_NUMBER}` : 'prod';
 
-const syncParams = {
-  localDir: publicDir,
+const s3Bucket = 'fabio-blog-dvc-us';
 
-  s3Params: {
-    Bucket: 'fabio-blog-dvc-us',
-    Prefix: s3Prefix
-  }
-};
+console.log({
+  AWS_REGION,
+  HEROKU_PR_NUMBER,
+  s3Bucket,
+  s3Prefix,
+  hasCreds: Boolean(AWS_ACCESS_KEY_ID && AWS_SECRET_ACCESS_KEY)
+});
 
 const syncCall = (method, ...args) =>
   new Promise((resolve, reject) => {
@@ -46,33 +47,71 @@ const syncCall = (method, ...args) =>
     synchroniser.on('end', resolve);
   });
 
-async function main() {
+async function prefixIsEmpty(prefix) {
+  try {
+    await s3.headObject({
+      Bucket: s3Bucket,
+      Prefix: prefix + '/index.html'
+    });
+  } catch (e) {
+    return true;
+  }
+  return false;
+}
+
+async function downloadFromS3(prefix) {
   // Removing mostly for local testing of this script:
   // heroku won't have public/ lying around.
-  await remove(publicDir);
+  // await remove(publicDir);
   try {
     console.time('download from s3');
-    await syncCall('downloadDir', syncParams);
+    await syncCall('downloadDir', {
+      localDir: publicDir,
+      s3Params: {
+        Bucket: s3Bucket,
+        Prefix: prefix
+      }
+    });
     console.timeEnd('download from s3');
   } catch (e) {
     console.error('Error downloading initial data');
     console.error(e);
     // Don't propagate. It's just a cache warming step
   }
+}
+
+async function uploadToS3() {
+  console.time('upload to s3');
+  await syncCall('uploadDir', {
+    localDir: publicDir,
+    s3Params: {
+      Bucket: s3Bucket,
+      Prefix: s3Prefix
+    }
+  });
+  console.timeEnd('upload to s3');
+}
+
+async function main() {
+  // First build of a PR is slow because it can't reuse cache.
+  // But we can download from prod to warm cache up.
+  const cacheWarmPrefix = (await prefixIsEmpty(s3Prefix)) ? 'prod' : s3Prefix;
+
+  await downloadFromS3(cacheWarmPrefix);
   try {
     run('yarn build');
-  } catch (e) {
+  } catch (buildError) {
     console.error('------------------------\n\n');
-    console.error('\n\n');
-    // Sometimes gatsby build fails because of a bad cache folder
-    // Clear it and try again
+    console.error(buildError);
+    console.error('\nAssuming bad cache and retrying:\n');
+    // Sometimes gatsby build fails because of bad cache.
+    // Clear it and try again.
     await remove(cacheDir);
+    await remove(publicDir);
     run('yarn build');
   }
   await move(path.join(publicDir, '404.html'), path.join(rootDir, '404.html'));
-  console.time('upload to s3');
-  await syncCall('uploadDir', syncParams);
-  console.timeEnd('upload to s3');
+  await uploadToS3();
   await remove(publicDir);
 }
 
